08f0cbb3a87509f2482595ac975517a9d05b155b
programhunter <hunter_pierce@outlook.com>
2019-06-27 09:04:36 -0400

    3 files modified, 9 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, NavbarComponent, WelcomeComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
